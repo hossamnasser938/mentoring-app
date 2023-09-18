6c526d8a662a18a4b160e8eff0ed228ea786e610
hossamnasser938 <hossamnasser938@gmail.com>
2023-09-18 07:18:53 +0300

    build bash script that automates creating resource

--- server/src/core/ioc-container/inversify.config.ts
+++ server/src/core/ioc-container/inversify.config.ts
@@ -1,3 +1,10 @@
+import { Container } from 'inversify';
+
+import { IOC_TYPES } from './types';
+
+export const iocContainer = new Container();
+
+/* mentor deps */
 import {
   MentorModel,
   mentorModel,
@@ -5,17 +12,11 @@ import {
 import { MentorDAO } from '@mentor/mentor.dao';
 import { IMentorDAO } from '@mentor/mentor.dao.abstract';
 import { MentorServie } from '@mentor/mentor.service';
-import { Container } from 'inversify';
 
-import { IOC_TYPES } from './types';
-
-const iocContainer = new Container();
-
-/* mentor deps */
 iocContainer.bind<IMentorDAO>(IOC_TYPES.IMentorDAO).to(MentorDAO);
 iocContainer.bind<MentorServie>(IOC_TYPES.MentorServie).to(MentorServie);
 iocContainer
   .bind<MentorModel>(IOC_TYPES.MentorModel)
   .toConstantValue(mentorModel);
 
-export { iocContainer, MentorDAO, MentorServie };
+export { MentorDAO, MentorServie };
